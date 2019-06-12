d018903d41e203f6ceb4a6a8cfe2e2ad60a30b89
rchuff <47576508+rchuff@users.noreply.github.com>
2019-06-12 15:13:41 -0400

    Final commit for drum kit

---
+++ index.js
@@ -0,0 +1,66 @@
+var buttonLength = document.querySelectorAll(".drum").length;
+
+//loop over length of button array and call the EventListener for each button
+for (var i = 0; i < buttonLength; i++) {
+  document.querySelectorAll(".drum")[i].addEventListener("click", function() {
+
+    var clicked = this.innerHTML;
+    drumHit(clicked);
+    buttonAnimation(clicked);
+
+  });
+}
+
+document.addEventListener("keydown", function(event) {
+  drumHit(event.key);
+  buttonAnimation(event.key);
+});
+
+//play corresponding drum kit noise
+function drumHit(keystroke) {
+  switch (keystroke) {
+    case "w":
+      var tom1 = new Audio("sounds/tom-1.mp3");
+      tom1.play();
+      break;
+    case "a":
+      var tom2 = new Audio("sounds/tom-2.mp3");
+      tom2.play();
+      break;
+    case "s":
+      var tom3 = new Audio("sounds/tom-3.mp3");
+      tom3.play();
+      break;
+    case "d":
+      var tom4 = new Audio("sounds/tom-4.mp3");
+      tom4.play();
+      break;
+    case "j":
+      var crash = new Audio("sounds/crash.mp3");
+      crash.play();
+      break;
+
+    case "k":
+      var kick = new Audio("sounds/kick-bass.mp3");
+      kick.play();
+      break;
+
+    case "l":
+      var snare = new Audio("sounds/snare.mp3");
+      snare.play();
+      break;
+
+    default:
+      console.log(this);
+
+  }
+}
+
+//pressed animation
+function buttonAnimation(currentKey) {
+  document.querySelector("." + currentKey).classList.add("pressed");
+
+  setTimeout(function() {
+    document.querySelector("." + currentKey).classList.remove("pressed");
+  }, 100);
+}
